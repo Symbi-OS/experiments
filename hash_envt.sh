61a70b9260bb3a16bc025ae9a2f655980014bbbd
FlareCoding <albert.slepak@gmail.com>
2022-12-04 13:27:44 -0500

    updated the system hashing script

--- hash_envt.sh
+++ hash_envt.sh
@@ -6,13 +6,16 @@ if [ "$1" != "-v" ]; then
 	CONF_VERBOSE=0
 fi
 
+# ----------------------------------------------------------- #
 
 KERNEL_IMAGE=/boot/vmlinuz-5.1.18-300.fc30.x86_64
 SYSTEM_MAP=/boot/System.map-5.1.18-300.fc30.x86_64
 
+EXPERIMENT_BINARIES=/bin/redis-server
+
 # ----------------------------------------------------------- #
 
-REQUIRED_HASHES=($KERNEL_IMAGE $SYSTEM_MAP)
+REQUIRED_HASHES=($KERNEL_IMAGE $SYSTEM_MAP $EXPERIMENT_BINARIES)
 
 # ----------------------------------------------------------- #
 
@@ -24,13 +27,15 @@ for entry in ${REQUIRED_HASHES[@]}; do
 
 	if [ $CONF_VERBOSE -eq 1 ]; then
 		#echo $entry -- $current_hash
-		printf '%s %-15s: %s\n' "$entry" "$current_hash"
+		printf '%-50s %s\n' "$entry" "$current_hash"
 	fi
 done
+if [ $CONF_VERBOSE -eq 1 ]; then echo ""; fi
 
 SYSTEM_HASH=$(echo "$RUNNING_HASH" | md5sum | grep -o "^\w*\b")
 
-echo "System Hash: $SYSTEM_HASH" |& tee system_hash
+echo "System Hash: $SYSTEM_HASH"
+echo "$SYSTEM_HASH" > system_hash
 
 
 
